68d73a65c871231991d8728f567d9e421a8b3c6e
Apostle-H <malinovski.nik@gmail.com>
2023-03-18 15:42:57 +0200

    levep part field rename

--- TopDown_Unity/Assets/_Source/LevelSystem/LevelPart.cs
+++ TopDown_Unity/Assets/_Source/LevelSystem/LevelPart.cs
@@ -6,20 +6,20 @@ namespace LevelSystem
 {
     public class LevelPart : MonoBehaviour
     {
-        [field: SerializeField] public bool TopSocket { get; private set; }
-        [field: SerializeField] public bool BottomSocket { get; private set; }
-        [field: SerializeField] public bool LeftSocket { get; private set; }
-        [field: SerializeField] public bool RightSocket { get; private set; }
+        [field: SerializeField] public bool TopPort { get; private set; }
+        [field: SerializeField] public bool BottomPort { get; private set; }
+        [field: SerializeField] public bool LeftPort { get; private set; }
+        [field: SerializeField] public bool RightPort { get; private set; }
         
         [field: SerializeField] public float Size { get; private set; } = 2.5f;
 
         public bool this[Direction direction] =>
             direction switch
             {
-                Direction.top => TopSocket,
-                Direction.bottom => BottomSocket,
-                Direction.left => LeftSocket,
-                Direction.right => RightSocket,
+                Direction.top => TopPort,
+                Direction.bottom => BottomPort,
+                Direction.left => LeftPort,
+                Direction.right => RightPort,
                 _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
             };
     }

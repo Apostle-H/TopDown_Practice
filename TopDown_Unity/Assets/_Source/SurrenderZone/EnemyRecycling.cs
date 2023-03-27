6eea6c5dd4ec5b185ad4f293571208158c3d1f19
dimastrik-78 <78548398+dimastrik-78@users.noreply.github.com>
2023-03-27 19:28:16 +0300

    Enemy recycling

---
+++ TopDown_Unity/Assets/_Source/SurrenderZone/EnemyRecycling.cs
@@ -0,0 +1,23 @@
+using System;
+using EntitySystem.Interactions;
+using UnityEngine;
+using Utils;
+
+namespace SurrenderZone
+{
+    public class EnemyRecycling : MonoBehaviour
+    {
+        [SerializeField] private LayerMask enemy;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (enemy.Contains(other.gameObject.layer) &&
+                other.gameObject.TryGetComponent(out IDraggable target) &&
+                target.IsDraggable)
+            {
+                Debug.Log("Resource + 1");
+                other.gameObject.SetActive(false);
+            }
+        }
+    }
+}

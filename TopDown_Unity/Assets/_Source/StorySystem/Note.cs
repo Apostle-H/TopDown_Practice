79b1e3e6b91e5b202ac435e166c56016dcd7aed0
dimastrik-78 <78548398+dimastrik-78@users.noreply.github.com>
2023-05-29 18:52:55 +0300

    Add note and dialog

---
+++ TopDown_Unity/Assets/_Source/StorySystem/Note.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using Utils;
+
+namespace StorySystem
+{
+    public class Note : MonoBehaviour
+    {
+        [SerializeField] private GameObject hint;
+        [SerializeField] private LayerMask player;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (player.Contains(other.gameObject.layer))
+            {
+                hint.SetActive(true);
+            }
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Читаем записку");
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (player.Contains(other.gameObject.layer))
+            {
+                hint.SetActive(false);
+            }
+        }
+    }
+}
---
+++ TopDown_Unity/Assets/_Source/TutorSystem/EducationalDialogues.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Utils;
+
+namespace TutorSystem
+{
+    public class EducationalDialogues : MonoBehaviour
+    {
+        [SerializeField] private GameObject dialog;
+        [SerializeField] private LayerMask player;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (player.Contains(other.gameObject.layer))
+            {
+                dialog.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (player.Contains(other.gameObject.layer))
+            {
+                dialog.SetActive(false);
+            }
+        }
+    }
+}
